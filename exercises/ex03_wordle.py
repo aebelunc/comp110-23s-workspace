4a93ad825b58cb2b3e150615ac00865d2dfee09c
Robert Ebel <aebel@ad.unc.edu>
2023-02-22 21:22:13 -0500

    Exercies 03 complete

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -6,12 +6,10 @@ def main() -> None:
     # variables for the main().
     SECRET: str = "codes"
     guess: str = ""
-    #  inserted a boolean value for win to track and exit program when guess is correct.
-    win: bool = False
     # made the turns a varibale so I could track the amount of turns you take in the game.
     turn: int = 1
     # while loop for our game. 
-    while turn <= 6 and not win:
+    while turn <= 6:
         # shows the amount of turns taken in the game. 
         print(f"=== Turn {turn}/6 ===")
         # made guess a variable equal to the input_guess def, calling it to make sure the guess has the right characters.
@@ -21,11 +19,11 @@ def main() -> None:
         print(guess_emoji)
         if guess == SECRET:
             print(f"You won in {turn} turns!")
-            win = True
-        # adding one to the turn after each loop. 
+            return False
+        # adding one to the turn after each loop.
         turn += 1
     # for when the guesses is up and you lose the game.
-    if not win:
+    if turn > 6:
         print("x/6 - Sorry, try again tomorrow!")
 # def for checking for indexes of the guessed word for matches.
 def contains_char(str_search: str, onechar_search: str, ) -> bool:
@@ -63,7 +61,7 @@ def emojified(guess: str, SECRET: str) -> str:
     # returns the search variable which includes the colored boxes.
     return(search)
 # def for the inputed guessed word and to check if it has the correct character length. 
-def input_guess(expected_length: int):
+def input_guess(expected_length: int) -> str:
     """Definition to input the guess to the game"""
     # starts off the game here with the input asking for the correct length of characters word.
     guess = input(f'Enter a {expected_length} character word: ')
